1be1e510ba1a167677d97c1117a2cbcd971bb07c
13Kain <98162781+13Kain@users.noreply.github.com>
2022-05-19 15:05:59 +0300

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Никита on 19.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
