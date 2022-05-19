9d3c9dee93c3189bbbda396c957f70b3a1d1d422
13Kain <98162781+13Kain@users.noreply.github.com>
2022-05-19 15:59:39 +0300

    preparing for the home task

--- StateAndDataFlow/ContentView.swift
+++ StateAndDataFlow/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var timer = TimeCounter()
+     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(timer.counter)")
+                .font(.largeTitle)
+                .padding(.top, 100)
+            Spacer()
+            ButtonView(timer: timer)
+            Spacer()
+        }
+            
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ButtonView: View {
+    var timer: TimeCounter
+    
+    var body: some View {
+        Button(action: { timer.startTimer() }) {
+            Text("Start")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+        }
+        .frame(width: 200, height: 60)
+        .background(.red)
+        .cornerRadius(20)
+        .overlay {
+            RoundedRectangle(cornerRadius: 20).stroke(.black, lineWidth: 4)
+        }
     }
 }
---
+++ StateAndDataFlow/TimeCounter.swift
@@ -0,0 +1,38 @@
+//
+//  TimeCounter.swift
+//  StateAndDataFlow
+//
+//  Created by Никита on 19.05.2022.
+//
+
+import Foundation
+import Combine
+
+class TimeCounter: ObservableObject {
+    let objectWillChange = PassthroughSubject<TimeCounter, Never>()
+    var counter = 3
+    var timer: Timer?
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(updateCounter),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    @objc private func updateCounter() {
+        if counter > 0 {
+            counter -= 1
+        } else {
+            killTimer()
+        }
+    }
+    
+    private func killTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+}
